cde140a4e5e5a05ec9e4cae9f9825ea02d7b1847
chinmaynawkar <119050434+chinmaynawkar@users.noreply.github.com>
2024-01-16 21:48:04 +0530

    week7-practice

---
+++ week-7/src/App.jsx
@@ -0,0 +1,47 @@
+import { useContext, useState } from "react"
+
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import countAtom from "./store/atoms/count";
+
+
+function App() {
+  
+  // wrap anyone that wants to use the teleported value inside a provider
+  // recoil, redux, Themes in mUI
+  return (
+    <div>
+      <RecoilRoot>
+        <Count  />
+   </RecoilRoot>
+    </div>
+  )
+}
+
+function Count() {
+  return <div>
+    <CountRenderer />
+    <Buttons  />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons() {
+  const [count , setCount] = useRecoilState(countAtom);
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count - 1)
+    }}>Decrease</button>
+  </div>
+}
+
+export default App
